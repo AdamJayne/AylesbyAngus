8121291d0c26e53d206619fda69318c40c1846e5
Adam Jayne <adamjayne402@gmail.com>
2017-09-15 23:38:06 -0400

    added more info, and a temp-background image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from '../src/app/home/home.component';
-import { AboutComponent } from '../src/app/about/about.component';
-import { HerdComponent } from '../src/app/herd/herd.component';
-import { PageNotFoundComponent } from '../src/app/page-not-found/page-not-found.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HerdComponent } from './herd/herd.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ForSaleComponent } from './for-sale/for-sale.component';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'sale', component: ForSaleComponent},
   { path: 'about', component: AboutComponent },
   { path: 'herd', component: HerdComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -22,7 +24,8 @@ const appRoutes: Routes = [
     HomeComponent,
     AboutComponent,
     HerdComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ForSaleComponent
   ],
   imports: [
     RouterModule.forRoot(
